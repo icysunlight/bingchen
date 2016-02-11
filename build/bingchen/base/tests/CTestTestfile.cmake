b381dc58eaaf7ad10728144eadde7aba3399c82e
icysunlight <691013542@qq.com>
2016-02-11 18:21:28 +0800

    delete *.swap tags build/

--- build/bingchen/base/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/code/bingchen/bingchen/base/tests
-# Build directory: /root/code/bingchen/build/bingchen/base/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
